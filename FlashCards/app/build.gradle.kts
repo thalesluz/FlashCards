6713a32108f879b0dda8a1164ac7d9715848550d
saviomesq <saviosouzamesquita@gmail.com>
2025-05-04 20:59:36 -0300

    Implementa agendamento de notificação conforme desempenho do usuário

--- FlashCards/app/build.gradle.kts
+++ FlashCards/app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // TimeComponent para notify
+    implementation ("androidx.work:work-runtime-ktx:2.9.0")
     
     // Room components
     implementation("androidx.room:room-runtime:2.6.1")
@@ -98,4 +101,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
 }
